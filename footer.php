98922dacd0c08ba9265ecc1b5e04b6a1541f6052
GwongZanMo <gwongzanmo@gmail.com>
2016-11-02 15:24:38 -0400

    create a footer.php for shared footer

--- footer.php
+++ footer.php
@@ -1,10 +1,8 @@
 <div id="bottom" class="padding-16 container" >
     <div class="row">
-        <div class="col-sm-4"></div>
-         <div id="footer" class="padding-16 col-sm-8">
+         <div id="footer" class="padding-16 col-sm-12">
             <hr>
             <p class="padding-16 center">this is a footer</p>
         </div>
     </div>
-   
 </div>
