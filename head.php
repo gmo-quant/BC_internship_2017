4a0b41a043fe9689c2255e75120be29a0bcef6fc
GwongZanMo <gwongzanmo@gmail.com>
2016-11-07 14:20:36 -0500

    refine bootstrap and jquery cdn link between <head>

--- head.php
+++ head.php
@@ -1,10 +1,5 @@
-<link rel="stylesheet" href="CSS/header.css">
-<link rel="stylesheet" href="CSS/share.css">
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
-<!-- jQuery library -->
+<meta charset="utf-8">
+				
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-
-<!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>	
