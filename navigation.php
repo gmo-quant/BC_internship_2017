979753c3e903ea0a437178ecf5b584bef2c91e9c
GwongZanMo <gwongzanmo@gmail.com>
2016-11-07 16:59:18 -0500

    sharable navigation frame for each Program degree

--- navigation.php
+++ navigation.php
@@ -1,34 +1,12 @@
-<div class="padding-16 topnav" style="position: relative; top:12px;">
-    <nav class="navbar">
-        <div class="container">
-
-            <ul class="nav padding-16 sky-mega-menu sky-mega-menu-anim-scale">
-                <li class="active">
-                    <a href="index.php">Home</a>
-                </li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"  aria-haspopup="true" aria-expanded="false">Program Map <span class="caret"></span></a>
-                    
-                    <ul class="dropdown-menu">
-                          <li><a href="#">Computer Programming and Analysis AS</a></li>
-                          <li><a href="#">Computer Programmer TC</a></li>
-                          <li><a href="#">Computer Programming Specialist TC</a></li>
-                    </ul>
-                  </li>
-
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">General Education Courses <span class="caret"></span></a>
-                    
-                    <ul class="dropdown-menu">
-                      <li><a href="#">Humanities</a></li>
-                      <li><a href="#">Social/Behavioral Science</a></li>
-                      <li><a href="#">Sciences</a></li>
-                    </ul>
-                </li>
-
-              <li><a href="#">Certifications & Careers</a></li>
-            </ul>
+<div class="container">
+        <div class="padding-16 topnav" style="position: relative; top:12px;">
+            <ul>
+                <li><a>Program Description</a></li>
+                <li><a>Program Map</a></li>
+                <li><a>Humanities Courses</a></li>
+                <li><a>Social/Behavioral Science Courses</a></li>
+                <li><a>Sciences Courses</a></li>
+            </ul>           
         </div>
-    </nav>
 </div>
 
