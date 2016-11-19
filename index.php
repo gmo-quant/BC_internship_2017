2fd0d7b8d332411e94ff74de06d7c63dbadc0c56
GwongZanMo <gwongzanmo@gmail.com>
2016-11-19 07:13:30 -0500

    add index.php for mock entrance to pages of a degree program

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <ul>
+            <li><a href="./degreeProgramFrame.php?pid=1">Computer Information Technology AS</a></li>
+            <li><a href="./degreeProgramFrame.php?pid=2">Programming Analysis</a></li>
+        </ul>
+    </body>
+</html>
