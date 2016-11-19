807985f3fc9cc09fb733c22396fff9274dd1043d
GwongZanMo <gwongzanmo@gmail.com>
2016-11-19 07:11:29 -0500

    add LG.php for login entrance

---
+++ navigation/LG.php
@@ -0,0 +1,26 @@
+<!--Login Form -->
+<div class="container">
+    <div class="row col-sm-4" >  
+    </div>
+    <div id="logindiv" class="col-sm-4">
+            <form class="form" action="#" id="login">
+
+                <label>User Name</label>
+                <br/>
+                <input type="text" id="username" placeholder="Ex -john123"/><br/>
+                <br/>
+                <label>password : </label>
+                <br/>
+                <input type="text" id="password" placeholder="************"/><br/>
+                <br/>
+                <input type="button" id="loginbtn" value="Login"/>
+                <input type="button" id="cancel" value="Cancel"/>
+                <br/>
+
+            </form>
+
+        </div> 
+    <div class="row col-sm-4">
+    </div>
+    
+</div>
